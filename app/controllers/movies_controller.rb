192aba990c09736f4288320640a22409e5589cdf
sema karan <karan18sema@gmail.com>
2024-02-29 10:24:13 +0100

    finish multisearch setup

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,13 +2,11 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     if params[:query].present?
-      sql_subquery = <<~SQL
-        movies.title @@ :query
-        OR movies.synopsis @@ :query
-        OR directors.first_name @@ :query
-        OR directors.last_name @@ :query
-      SQL
-      @movies = @movies.joins(:director).where(sql_subquery, query: "%#{params[:query]}%")
+      @movies = PgSearch.multisearch(params[:query])
+      # @movies.map do |result|
+      #   puts result.searchable.title
+      # end
+      @movies
     end
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,5 @@
 class Movie < ApplicationRecord
   belongs_to :director
   include PgSearch::Model
-  pg_search_scope :global_search,
-    against: [ :title, :synopsis ],
-    associated_against: {
-      director: [ :first_name, :last_name ]
-    },
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
-
+  multisearchable against: [:title, :synopsis]
 end
